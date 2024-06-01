c706f21cfefefa29085616a7544b2358fa9859af
charSLee013 <aaa7353@live.com>
2024-06-02 01:50:42 +0800

    fix bug

--- demo.py
+++ demo.py
@@ -41,14 +41,14 @@ params_infer_code = {
 params_refine_text = {'prompt': '[oral_5][laugh_1][break_3]'}
 
 texts = ["So we found being competitive and collaborative was a huge way of staying motivated towards our goals, so one person to call when you fall off, one person who gets you back on then one person to actually do the activity with",
-        #  "游戏发生在一个被称作「提瓦特」的幻想世界，在这里，被神选中的人将被授予「神之眼」，导引元素之力。",
-        #  "你将扮演一位名为「旅行者」的神秘角色，在自由的旅行中邂逅性格各异、能力独特的同伴们，和他们一起击败强敌，找回失散的亲人——同时，逐步发掘「原神」的真相。",
-        #  "因为你的素养很差，我现在每天玩原神都能赚150原石，每个月差不多5000原石的收入， 也就是现实生活中每个月5000美元的收入水平，换算过来最少也30000人民币，",
-        #  "虽然我只有14岁，但是已经超越了中国绝大多数人(包括你)的水平，这便是原神给我的骄傲的资本。",
-        #  "毫不夸张地说，《原神》是miHoYo迄今为止规模最为宏大，也是最具野心的一部作品。",
-        #  "即便在经历了8700个小时的艰苦战斗后，游戏还有许多尚未发现的秘密，错过的武器与装备，以及从未使用过的法术和技能。",
-        #  "尽管游戏中的战斗体验和我们之前在烧机系列游戏所见到的没有多大差别，但游戏中各类精心设计的敌人以及Boss战已然将战斗抬高到了一个全新的水平。",
-        #  "就和几年前的《 塞尔达传说 》一样，《原神》也是一款能够推动同类游戏向前发展的优秀作品。",
+         "游戏发生在一个被称作「提瓦特」的幻想世界，在这里，被神选中的人将被授予「神之眼」，导引元素之力。",
+         "你将扮演一位名为「旅行者」的神秘角色，在自由的旅行中邂逅性格各异、能力独特的同伴们，和他们一起击败强敌，找回失散的亲人——同时，逐步发掘「原神」的真相。",
+         "因为你的素养很差，我现在每天玩原神都能赚150原石，每个月差不多5000原石的收入， 也就是现实生活中每个月5000美元的收入水平，换算过来最少也30000人民币，",
+         "虽然我只有14岁，但是已经超越了中国绝大多数人(包括你)的水平，这便是原神给我的骄傲的资本。",
+         "毫不夸张地说，《原神》是miHoYo迄今为止规模最为宏大，也是最具野心的一部作品。",
+         "即便在经历了8700个小时的艰苦战斗后，游戏还有许多尚未发现的秘密，错过的武器与装备，以及从未使用过的法术和技能。",
+         "尽管游戏中的战斗体验和我们之前在烧机系列游戏所见到的没有多大差别，但游戏中各类精心设计的敌人以及Boss战已然将战斗抬高到了一个全新的水平。",
+         "就和几年前的《 塞尔达传说 》一样，《原神》也是一款能够推动同类游戏向前发展的优秀作品。",
         ]
 
 # 对文本进行预处理
@@ -67,10 +67,11 @@ def filter_punctuation(text):
 for t in texts:
     filter_text = filter_punctuation(t)
     # 调用模型显示实际生存的文本
-    filter_text = chat.infer(
+    filter_text:str = chat.infer(
         text=filter_text, skip_refine_text=False, refine_text_only=True,
         params_refine_text=params_refine_text,
         params_infer_code=params_infer_code)[0]
+    filter_text = filter_text.replace(" ","")
     logging.info(f"输入文本: {t}\n预处理后的文本: {filter_text}")
     new_texts.append(filter_text)
 
@@ -96,11 +97,6 @@ combined_wavs = np.concatenate(all_wavs, axis=1)
 #     wf.setsampwidth(2)
 #     wf.setframerate(24000)
 #     wf.writeframes(audio_data_int16)
-
-# Perform inference and play the generated audio
-wavs = chat.infer(texts)
-Audio(wavs[0], rate=24_000, autoplay=True)
-
 
 # # Save the generated audio 
 torchaudio.save("output.wav", torch.from_numpy(combined_wavs), 24000)
